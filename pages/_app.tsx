7f373c1b49bc04aaa47bfd34755f22b273ecf569
Suranjan <suranjanbiswas2002@gmail.com>
2021-06-09 01:27:04 +0530

    fix: fixed the metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React App</title>
+        <title>Meetups</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
