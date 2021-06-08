16c10243854fee043ca824219aaca94328b9c7b6
Suranjan <suranjanbiswas2002@gmail.com>
2021-06-09 01:26:01 +0530

    add: metadata added to _ap.tsx file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import "../styles/globals.css";
 import { AppProps } from "next/app";
+import Head from "next/head";
 import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>React App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
