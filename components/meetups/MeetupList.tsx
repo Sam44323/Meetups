97f8256c2d1dc18a7d66126edd23761ea3e86686
Suranjan <suranjanbiswas2002@gmail.com>
2021-06-09 02:33:25 +0530

    add: meetups folder added

---
+++ components/meetups/MeetupList.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./MeetupList.module.scss";
+import MeetupItem from "./MeetupItem";
+
+const MeetupList: React.FC<{
+  meetups: { id: number; image: string; title: string; address: string }[];
+}> = (props) => {
+  return (
+    <ul className={styles.list}>
+      {props.meetups.map((meetup) => (
+        <MeetupItem
+          key={meetup.id}
+          id={meetup.id}
+          image={meetup.image}
+          title={meetup.title}
+          address={meetup.address}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default MeetupList;
---
+++ components/meetups/NewMeetupForm.tsx
@@ -0,0 +1,63 @@
+import React, { useRef } from "react";
+import styles from "./NewMeetupForm.module.scss";
+import Card from "../ui/Card";
+
+const NewMeetupForm: React.FC<{ onAddMeetup: (data: any) => void }> = (
+  props
+) => {
+  const titleInputRef = useRef<HTMLInputElement | null>(null);
+  const imageInputRef = useRef<HTMLInputElement | null>(null);
+  const addressInputRef = useRef<HTMLInputElement | null>(null);
+  const descriptionInputRef = useRef<HTMLTextAreaElement | null>(null);
+
+  function submitHandler(event: { preventDefault: () => void }) {
+    event.preventDefault();
+
+    const enteredTitle = titleInputRef.current && titleInputRef.current.value;
+    const enteredImage = imageInputRef.current && imageInputRef.current.value;
+    const enteredAddress =
+      addressInputRef.current && addressInputRef.current.value;
+    const enteredDescription =
+      descriptionInputRef.current && descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+
+    props.onAddMeetup(meetupData);
+  }
+
+  return (
+    <Card>
+      <form className={styles.form} onSubmit={submitHandler}>
+        <div className={styles.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" ref={titleInputRef} />
+        </div>
+        <div className={styles.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" ref={imageInputRef} />
+        </div>
+        <div className={styles.control}>
+          <label htmlFor="address">Address</label>
+          <input type="text" required id="address" ref={addressInputRef} />
+        </div>
+        <div className={styles.control}>
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            required
+            rows={5}
+            ref={descriptionInputRef}
+          ></textarea>
+        </div>
+        <div className={styles.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
+};
