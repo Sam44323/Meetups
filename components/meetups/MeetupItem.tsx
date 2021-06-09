eac3b8d8910596f610fd5ccf08f6ec9d1cb6a1f3
Suranjan <suranjanbiswas2002@gmail.com>
2021-06-10 01:16:14 +0530

    update: updated the MeetupItem.tsx file

--- components/meetups/MeetupItem.tsx
+++ components/meetups/MeetupItem.tsx
@@ -2,24 +2,28 @@ import React from "react";
 import styles from "./MeetupItem.module.css";
 import Card from "../ui/Card";
 
-const MeetupItem: React.FC<{ image: string; title: string; address: string }> =
-  (props) => {
-    return (
-      <li className={styles.item}>
-        <Card>
-          <div className={styles.image}>
-            <img src={props.image} alt={props.title} />
-          </div>
-          <div className={styles.content}>
-            <h3>{props.title}</h3>
-            <address>{props.address}</address>
-          </div>
-          <div className={styles.actions}>
-            <button>Show Details</button>
-          </div>
-        </Card>
-      </li>
-    );
-  };
+const MeetupItem: React.FC<{
+  image: string;
+  title: string;
+  address: string;
+  id: number;
+}> = (props) => {
+  return (
+    <li className={styles.item}>
+      <Card>
+        <div className={styles.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className={styles.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+        </div>
+        <div className={styles.actions}>
+          <button>Show Details</button>
+        </div>
+      </Card>
+    </li>
+  );
+};
 
 export default MeetupItem;
