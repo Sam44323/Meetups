d1a7e1d37f5bbb005de38eaaa505bd7eb1743c75
Suranjan <suranjanbiswas2002@gmail.com>
2021-06-10 01:10:36 +0530

    add: new-meetup page created

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Meetup: React.FC = () => (
+  <>
+    <h1>Meetup</h1>
+  </>
+);
+
+export default Meetup;
---
+++ pages/new-meetup/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NewMeetup: React.FC = () => (
+  <>
+    <h1>New Meetup</h1>
+  </>
+);
+
+export default NewMeetup;
