756f3282f682ac1b4f6705c9f430d50c19a37593
Suranjan <suranjanbiswas2002@gmail.com>
2021-06-10 01:38:12 +0530

    add: interface added for the addMeetupHandler parameter data

--- pages/new-meetup/index.tsx
+++ pages/new-meetup/index.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
-const NewMeetup: React.FC = () => (
-  <>
-    <NewMeetupForm
-      onAddMeetup={(data) => {
-        console.log(data);
-      }}
-    />
-  </>
-);
+interface MeetupData {
+  title: string;
+  image: string;
+  address: string;
+  description: string;
+}
+
+const NewMeetup: React.FC = () => {
+  const addMeetupHandler = React.useCallback((data: MeetupData) => {
+    console.log(data);
+  }, []);
+  return (
+    <>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </>
+  );
+};
 
 export default NewMeetup;
