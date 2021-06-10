a4eb4cf789378605ea05bb36f799d4d49afc1c93
Suranjan <suranjanbiswas2002@gmail.com>
2021-06-10 17:44:01 +0530

    add: connections added to database and basic project is complete

--- pages/api/new-meetup.ts
+++ pages/api/new-meetup.ts
@@ -1,9 +1,22 @@
 import { MongoClient } from "mongodb";
 
-const Handler = (req: Request, res: Response) => {
+const Handler = async (req: any, res: any) => {
   if (req.method === "POST") {
     const data = req.body;
     console.log(data);
+    const client = await MongoClient.connect(
+      "mongodb://127.0.0.1:27017/Meetups"
+    );
+    if (client) {
+      const db = client.db();
+      const meetupsCollections = db.collection("meetups");
+      const result = await meetupsCollections.insertOne(data);
+      console.log(result);
+      client.close();
+      res.status(201).json({
+        message: "Added a new meetup!",
+      });
+    }
   }
 };
 
